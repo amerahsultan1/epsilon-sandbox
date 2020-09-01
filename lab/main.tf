25233dcc83ec9094287eabf746df27b4993a0e0b
amerahsultan1 <amerah_sultan@hotmail.com>
2020-09-02 00:22:54 +0300

    epsilon sandbox

---
+++ lab/main.tf
@@ -0,0 +1,141 @@
+provider "random" {}
+
+module "tags_network" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git"
+  namespace   = var.name
+  environment = "dev"
+  name        = "devops-bootcamp"
+  delimiter   = "_"
+
+  tags = {
+    owner = var.name
+    type  = "network"
+  }
+}
+
+module "tags_sandbox" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git"
+  namespace   = var.name
+  environment = "dev"
+  name        = "sandbox-devops-bootcamp"
+  delimiter   = "_"
+
+  tags = {
+    owner = var.name
+    type  = "sandbox"
+  }
+}
+
+resource "aws_vpc" "k8s_lab" {
+  cidr_block           = "10.0.0.0/16"
+  tags                 = module.tags_network.tags
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "lab_gateway" {
+  vpc_id = aws_vpc.k8s_lab.id
+  tags   = module.tags_network.tags
+}
+
+resource "aws_route" "lab_internet_access" {
+  route_table_id         = aws_vpc.k8s_lab.main_route_table_id
+  gateway_id             = aws_internet_gateway.lab_gateway.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "sandbox" {
+  vpc_id                  = aws_vpc.k8s_lab.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  tags                    = module.tags_sandbox.tags
+}
+
+resource "aws_security_group" "sandbox" {
+  vpc_id = aws_vpc.k8s_lab.id
+  tags   = module.tags_sandbox.tags
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "random_id" "keypair" {
+  keepers = {
+    public_key = file(var.public_key_path)
+  }
+
+  byte_length = 8
+}
+
+resource "aws_key_pair" "lab_keypair" {
+  key_name   = format("%s_keypair_%s", var.name, random_id.keypair.hex)
+  public_key = random_id.keypair.keepers.public_key
+}
+
+data "aws_ami" "latest_sandbox" {
+  most_recent = true
+  owners      = ["772816346052"]
+
+  filter {
+    name   = "name"
+    values = ["epsilon-sandbox*"]
+  }
+}
+
+resource "aws_instance" "sandbox" {
+  ami                    = data.aws_ami.latest_sandbox.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.sandbox.id
+  vpc_security_group_ids = [aws_security_group.sandbox.id]
+  key_name               = aws_key_pair.lab_keypair.id
+
+  root_block_device {
+    volume_size = 100
+    volume_type = "gp2"
+  }
+
+  tags = module.tags_sandbox.tags
+}
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+name             = "epsilon"
+key_name         = "lab-key"
+private_key_path = "./ssh/id_rsa"
+public_key_path  = "./ssh/id_rsa.pub"
+instance_type    = "t3.xlarge"
